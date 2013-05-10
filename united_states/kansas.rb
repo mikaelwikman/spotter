596509b68030970934d4a368a0d6815451467c54
Mikael Wikman <mikael@swedcontent.com>
2013-05-10 11:59:31 +0200

    kansas

--- united_states/kansas.rb
+++ united_states/kansas.rb
@@ -3,6 +3,432 @@ match 'kansas', w(/(us )?ks/) do
   set subregion: 'kansas'
   set subregion_type: 'state'
 
+  match 'allen' do
+    set subsubregion: 'allen'
+    include_subfolder 'kansas/allen'
+  end
+  match 'anderson' do
+    set subsubregion: 'anderson'
+    include_subfolder 'kansas/anderson'
+  end
+  match 'atchison' do
+    set subsubregion: 'atchison'
+    include_subfolder 'kansas/atchison'
+  end
+  match 'barber' do
+    set subsubregion: 'barber'
+    include_subfolder 'kansas/barber'
+  end
+  match 'barton' do
+    set subsubregion: 'barton'
+    include_subfolder 'kansas/barton'
+  end
+  match 'bourbon' do
+    set subsubregion: 'bourbon'
+    include_subfolder 'kansas/bourbon'
+  end
+  match 'brown' do
+    set subsubregion: 'brown'
+    include_subfolder 'kansas/brown'
+  end
+  match 'butler' do
+    set subsubregion: 'butler'
+    include_subfolder 'kansas/butler'
+  end
+  match 'chase' do
+    set subsubregion: 'chase'
+    include_subfolder 'kansas/chase'
+  end
+  match 'chautauqua' do
+    set subsubregion: 'chautauqua'
+    include_subfolder 'kansas/chautauqua'
+  end
+  match 'cherokee' do
+    set subsubregion: 'cherokee'
+    include_subfolder 'kansas/cherokee'
+  end
+  match 'cheyenne' do
+    set subsubregion: 'cheyenne'
+    include_subfolder 'kansas/cheyenne'
+  end
+  match 'clark' do
+    set subsubregion: 'clark'
+    include_subfolder 'kansas/clark'
+  end
+  match 'clay' do
+    set subsubregion: 'clay'
+    include_subfolder 'kansas/clay'
+  end
+  match 'cloud' do
+    set subsubregion: 'cloud'
+    include_subfolder 'kansas/cloud'
+  end
+  match 'coffey' do
+    set subsubregion: 'coffey'
+    include_subfolder 'kansas/coffey'
+  end
+  match 'comanche' do
+    set subsubregion: 'comanche'
+    include_subfolder 'kansas/comanche'
+  end
+  match 'cowley' do
+    set subsubregion: 'cowley'
+    include_subfolder 'kansas/cowley'
+  end
+  match 'crawford' do
+    set subsubregion: 'crawford'
+    include_subfolder 'kansas/crawford'
+  end
+  match 'decatur' do
+    set subsubregion: 'decatur'
+    include_subfolder 'kansas/decatur'
+  end
+  match 'dickinson' do
+    set subsubregion: 'dickinson'
+    include_subfolder 'kansas/dickinson'
+  end
+  match 'doniphan' do
+    set subsubregion: 'doniphan'
+    include_subfolder 'kansas/doniphan'
+  end
+  match 'douglas' do
+    set subsubregion: 'douglas'
+    include_subfolder 'kansas/douglas'
+  end
+  match 'edwards' do
+    set subsubregion: 'edwards'
+    include_subfolder 'kansas/edwards'
+  end
+  match 'elk' do
+    set subsubregion: 'elk'
+    include_subfolder 'kansas/elk'
+  end
+  match 'ellis' do
+    set subsubregion: 'ellis'
+    include_subfolder 'kansas/ellis'
+  end
+  match 'ellsworth' do
+    set subsubregion: 'ellsworth'
+    include_subfolder 'kansas/ellsworth'
+  end
+  match 'finney' do
+    set subsubregion: 'finney'
+    include_subfolder 'kansas/finney'
+  end
+  match 'ford' do
+    set subsubregion: 'ford'
+    include_subfolder 'kansas/ford'
+  end
+  match 'franklin' do
+    set subsubregion: 'franklin'
+    include_subfolder 'kansas/franklin'
+  end
+  match 'geary' do
+    set subsubregion: 'geary'
+    include_subfolder 'kansas/geary'
+  end
+  match 'gove' do
+    set subsubregion: 'gove'
+    include_subfolder 'kansas/gove'
+  end
+  match 'graham' do
+    set subsubregion: 'graham'
+    include_subfolder 'kansas/graham'
+  end
+  match 'grant' do
+    set subsubregion: 'grant'
+    include_subfolder 'kansas/grant'
+  end
+  match 'gray' do
+    set subsubregion: 'gray'
+    include_subfolder 'kansas/gray'
+  end
+  match 'greeley' do
+    set subsubregion: 'greeley'
+    include_subfolder 'kansas/greeley'
+  end
+  match 'greenwood' do
+    set subsubregion: 'greenwood'
+    include_subfolder 'kansas/greenwood'
+  end
+  match 'hamilton' do
+    set subsubregion: 'hamilton'
+    include_subfolder 'kansas/hamilton'
+  end
+  match 'harper' do
+    set subsubregion: 'harper'
+    include_subfolder 'kansas/harper'
+  end
+  match 'harvey' do
+    set subsubregion: 'harvey'
+    include_subfolder 'kansas/harvey'
+  end
+  match 'haskell' do
+    set subsubregion: 'haskell'
+    include_subfolder 'kansas/haskell'
+  end
+  match 'hodgeman' do
+    set subsubregion: 'hodgeman'
+    include_subfolder 'kansas/hodgeman'
+  end
+  match 'jackson' do
+    set subsubregion: 'jackson'
+    include_subfolder 'kansas/jackson'
+  end
+  match 'jefferson' do
+    set subsubregion: 'jefferson'
+    include_subfolder 'kansas/jefferson'
+  end
+  match 'jewell' do
+    set subsubregion: 'jewell'
+    include_subfolder 'kansas/jewell'
+  end
+  match 'johnson' do
+    set subsubregion: 'johnson'
+    include_subfolder 'kansas/johnson'
+  end
+  match 'kearny' do
+    set subsubregion: 'kearny'
+    include_subfolder 'kansas/kearny'
+  end
+  match 'kingman' do
+    set subsubregion: 'kingman'
+    include_subfolder 'kansas/kingman'
+  end
+  match 'kiowa' do
+    set subsubregion: 'kiowa'
+    include_subfolder 'kansas/kiowa'
+  end
+  match 'labette' do
+    set subsubregion: 'labette'
+    include_subfolder 'kansas/labette'
+  end
+  match 'lane' do
+    set subsubregion: 'lane'
+    include_subfolder 'kansas/lane'
+  end
+  match 'leavenworth' do
+    set subsubregion: 'leavenworth'
+    include_subfolder 'kansas/leavenworth'
+  end
+  match 'lincoln' do
+    set subsubregion: 'lincoln'
+    include_subfolder 'kansas/lincoln'
+  end
+  match 'linn' do
+    set subsubregion: 'linn'
+    include_subfolder 'kansas/linn'
+  end
+  match 'logan' do
+    set subsubregion: 'logan'
+    include_subfolder 'kansas/logan'
+  end
+  match 'lyon' do
+    set subsubregion: 'lyon'
+    include_subfolder 'kansas/lyon'
+  end
+  match 'marion' do
+    set subsubregion: 'marion'
+    include_subfolder 'kansas/marion'
+  end
+  match 'marshall' do
+    set subsubregion: 'marshall'
+    include_subfolder 'kansas/marshall'
+  end
+  match 'mcpherson' do
+    set subsubregion: 'mcpherson'
+    include_subfolder 'kansas/mcpherson'
+  end
+  match 'meade' do
+    set subsubregion: 'meade'
+    include_subfolder 'kansas/meade'
+  end
+  match 'miami' do
+    set subsubregion: 'miami'
+    include_subfolder 'kansas/miami'
+  end
+  match 'mitchell' do
+    set subsubregion: 'mitchell'
+    include_subfolder 'kansas/mitchell'
+  end
+  match 'montgomery' do
+    set subsubregion: 'montgomery'
+    include_subfolder 'kansas/montgomery'
+  end
+  match 'morris' do
+    set subsubregion: 'morris'
+    include_subfolder 'kansas/morris'
+  end
+  match 'morton' do
+    set subsubregion: 'morton'
+    include_subfolder 'kansas/morton'
+  end
+  match 'nemaha' do
+    set subsubregion: 'nemaha'
+    include_subfolder 'kansas/nemaha'
+  end
+  match 'neosho' do
+    set subsubregion: 'neosho'
+    include_subfolder 'kansas/neosho'
+  end
+  match 'ness' do
+    set subsubregion: 'ness'
+    include_subfolder 'kansas/ness'
+  end
+  match 'norton' do
+    set subsubregion: 'norton'
+    include_subfolder 'kansas/norton'
+  end
+  match 'osage' do
+    set subsubregion: 'osage'
+    include_subfolder 'kansas/osage'
+  end
+  match 'osborne' do
+    set subsubregion: 'osborne'
+    include_subfolder 'kansas/osborne'
+  end
+  match 'ottawa' do
+    set subsubregion: 'ottawa'
+    include_subfolder 'kansas/ottawa'
+  end
+  match 'pawnee' do
+    set subsubregion: 'pawnee'
+    include_subfolder 'kansas/pawnee'
+  end
+  match 'phillips' do
+    set subsubregion: 'phillips'
+    include_subfolder 'kansas/phillips'
+  end
+  match 'pottawatomie' do
+    set subsubregion: 'pottawatomie'
+    include_subfolder 'kansas/pottawatomie'
+  end
+  match 'pratt' do
+    set subsubregion: 'pratt'
+    include_subfolder 'kansas/pratt'
+  end
+  match 'rawlins' do
+    set subsubregion: 'rawlins'
+    include_subfolder 'kansas/rawlins'
+  end
+  match 'reno' do
+    set subsubregion: 'reno'
+    include_subfolder 'kansas/reno'
+  end
+  match 'republic' do
+    set subsubregion: 'republic'
+    include_subfolder 'kansas/republic'
+  end
+  match 'rice' do
+    set subsubregion: 'rice'
+    include_subfolder 'kansas/rice'
+  end
+  match 'riley' do
+    set subsubregion: 'riley'
+    include_subfolder 'kansas/riley'
+  end
+  match 'rooks' do
+    set subsubregion: 'rooks'
+    include_subfolder 'kansas/rooks'
+  end
+  match 'rush' do
+    set subsubregion: 'rush'
+    include_subfolder 'kansas/rush'
+  end
+  match 'russell' do
+    set subsubregion: 'russell'
+    include_subfolder 'kansas/russell'
+  end
+  match 'saline' do
+    set subsubregion: 'saline'
+    include_subfolder 'kansas/saline'
+  end
+  match 'scott' do
+    set subsubregion: 'scott'
+    include_subfolder 'kansas/scott'
+  end
+  match 'sedgwick' do
+    set subsubregion: 'sedgwick'
+    include_subfolder 'kansas/sedgwick'
+  end
+  match 'seward' do
+    set subsubregion: 'seward'
+    include_subfolder 'kansas/seward'
+  end
+  match 'shawnee' do
+    set subsubregion: 'shawnee'
+    include_subfolder 'kansas/shawnee'
+  end
+  match 'sheridan' do
+    set subsubregion: 'sheridan'
+    include_subfolder 'kansas/sheridan'
+  end
+  match 'sherman' do
+    set subsubregion: 'sherman'
+    include_subfolder 'kansas/sherman'
+  end
+  match 'smith' do
+    set subsubregion: 'smith'
+    include_subfolder 'kansas/smith'
+  end
+  match 'stafford' do
+    set subsubregion: 'stafford'
+    include_subfolder 'kansas/stafford'
+  end
+  match 'stanton' do
+    set subsubregion: 'stanton'
+    include_subfolder 'kansas/stanton'
+  end
+  match 'stevens' do
+    set subsubregion: 'stevens'
+    include_subfolder 'kansas/stevens'
+  end
+  match 'sumner' do
+    set subsubregion: 'sumner'
+    include_subfolder 'kansas/sumner'
+  end
+  match 'thomas' do
+    set subsubregion: 'thomas'
+    include_subfolder 'kansas/thomas'
+  end
+  match 'trego' do
+    set subsubregion: 'trego'
+    include_subfolder 'kansas/trego'
+  end
+  match 'wabaunsee' do
+    set subsubregion: 'wabaunsee'
+    include_subfolder 'kansas/wabaunsee'
+  end
+  match 'wallace' do
+    set subsubregion: 'wallace'
+    include_subfolder 'kansas/wallace'
+  end
+  match 'washington' do
+    set subsubregion: 'washington'
+    include_subfolder 'kansas/washington'
+  end
+  match 'wichita' do
+    set subsubregion: 'wichita'
+    include_subfolder 'kansas/wichita'
+  end
+  match 'wilson' do
+    set subsubregion: 'wilson'
+    include_subfolder 'kansas/wilson'
+  end
+  match 'woodson' do
+    set subsubregion: 'woodson'
+    include_subfolder 'kansas/woodson'
+  end
+  match 'wyandotte' do
+    set subsubregion: 'wyandotte'
+    include_subfolder 'kansas/wyandotte'
+  end
+
+
+
+
+  ##############
+
   match 'cuba' do
     set place: 'cuba'
   end
